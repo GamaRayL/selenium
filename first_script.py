83deaf020570ebf29b1617c0228d4bea1bda0e0d
GamaRayL <gamaizingg@gmail.com>
2024-09-04 14:53:48 +0300

    Разделение на фикстуру + несколько тестов

--- first_script.py
+++ first_script.py
@@ -4,26 +4,37 @@ from selenium.webdriver.common.by import By
 
 
 def test_title(driver):
-
     title = driver.title
     assert title == 'Web form', f"Expected title to be 'Web form', but got {title}"
 
     print(title)
 
-    # driver.implicitly_wait(0.5)
-    #
-    # text_box = driver.find_element(by=By.NAME, value='my-text')
-    # submit_button = driver.find_element(by=By.CSS_SELECTOR, value='button')
-    #
-    # text_box.send_keys('Selenium')
-    # submit_button.click()
-    #
-    # message = driver.find_element(by=By.ID, value='message')
-    # value = message.text
-    # assert value == 'Received!'
+
+def test_form_submission(driver):
+    text_box = driver.find_element(by=By.NAME, value='my-text')
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value='button')
+
+    text_box.send_keys('Selenium')
+    submit_button.click()
+
+    message = driver.find_element(by=By.ID, value='message')
+    value = message.text
+    assert value == 'Received!', f'Ожидаемый ответ "Received!", но получен {value}'
 
 
-@pytest.fixture
+def test_empty_submission(driver):
+    submit_button = driver.find_element(by=By.CLASS_NAME, value='btn')
+    submit_button.click()
+
+    message = driver.find_element(by=By.ID, value='message')
+    value = message.text
+
+    assert value == 'Received!', f'Ожидаемый ответ "Please enter a value", но получен {value}'
+
+
+#   Please enter a value
+
+@pytest.fixture()
 def driver():
     driver = webdriver.Chrome()
     driver.get('https://www.selenium.dev/selenium/web/web-form.html')
